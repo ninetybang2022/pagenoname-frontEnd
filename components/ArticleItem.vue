c8ad868acc38d22a6e42c643c5231f0fe4bc0f7c
赖文邦 <ninetybang@gmail.com>
2018-03-14 23:49:00 +0800

    修改部分样式搜索响应式样式

---
+++ components/ArticleItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <article class="blog-article-item">
+        <h2 class="title">Flex 布局教程：语法篇</h2>
+    </article>
+</template>
+<script>
+    export default {
+        name: 'ArticleItem'
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
 <template>
     <section class="container">
+        <div class="blog-page-index">
+            <div class="article-list">
+                <article-item></article-item>
+            </div>
+            <aside class="blog-aside">
+                <div class="aside-search">
+                    <blog-search></blog-search>
+                </div>
+            </aside>
+        </div>
     </section>
 </template>
 <script>
+    import ArticleItem from '~/components/ArticleItem'
+    import BlogSearch from '~/components/BlogSearch'
 
     export default {
-        name: 'PageIndex'
+        name: 'PageIndex',
+        components: {
+            ArticleItem,
+            BlogSearch
+        }
     }
 </script>
